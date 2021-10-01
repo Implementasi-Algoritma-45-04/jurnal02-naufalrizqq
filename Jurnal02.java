63a52328ab3d16f4cb785e2f09144a9cfb5b1611
Naufal Rizq Ramadhan <naufalrizq100@gmail.com>
2021-10-01 15:33:33 +0700

    Solusi Jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+public static void main(String[] args) {
+	System.out.println("NIM   : 6706213150");
+	System.out.println("Nama  : Naufal Rizq Ramadhan");
+	System.out.println("Kelas : D3IF-45-04");
+	}
+	}
